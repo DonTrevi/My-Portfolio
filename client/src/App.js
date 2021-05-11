8f571c053a26eb8790d82f7b104fd5621e8afb2b
DonTrevi <valentin.trevi@gmail.com>
2021-05-11 01:10:44 -0300

    Valentin Trevisiol Montiel

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar.js'
+
+
+
+
+export default function App() {
+
+  return (
+    <NavBar />
+
+  );
+}
